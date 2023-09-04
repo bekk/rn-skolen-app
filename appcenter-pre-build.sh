21529998765f9bbf4a35c2f823eeaf7bd93d30ff
Leif Erik Bjørkli <leiferikbjorkli@gmail.com>
2023-09-04 22:08:32 +0200

    Justerer script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Sett versjonen på iOS
-if [ "$APPCENTER_BRANCH" != "master" ]; then
-  echo "Setting iOS version to $APPCENTER_BRANCH"
-  plutil -replace CFBundleShortVersionString -string "$APPCENTER_BRANCH" $APPCENTER_SOURCE_DIRECTORY/ios/RNSkolenApp/Info.plist
-fi
 
-# Sett versjonen på Android
 if [ "$APPCENTER_BRANCH" != "master" ]; then
-  echo "Setting Android version to $APPCENTER_BRANCH"
-  sed -i "s/versionName \".*\"/versionName \"$APPCENTER_BRANCH\"/" $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
+  if [ -n "$APPCENTER_XCODE_PROJECT" ]; then
+    echo "Setting iOS version to $APPCENTER_BRANCH"
+    plutil -replace CFBundleShortVersionString -string "$APPCENTER_BRANCH" $APPCENTER_SOURCE_DIRECTORY/ios/RNSkolenApp/Info.plist
+  fi
+  if [ -n "$APPCENTER_ANDROID_VARIANT" ]; then
+    echo "Setting Android version to $APPCENTER_BRANCH"
+    sed -i '' "s/versionName \".*\"/versionName \"$APPCENTER_BRANCH\"/" $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
+  fi
 fi
