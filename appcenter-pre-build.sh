df27ced67743534fe17a7a2ad1581242520fa287
Leif Erik Bjørkli <leiferikbjorkli@gmail.com>
2023-09-04 21:40:14 +0200

    Legger til APPCENTER_SOURCE_DIRECTORY

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,11 +3,11 @@
 # Sett versjonen på iOS
 if [ "$APPCENTER_BRANCH" != "master" ]; then
   echo "Setting iOS version to $APPCENTER_BRANCH"
-  plutil -replace CFBundleShortVersionString -string "$APPCENTER_BRANCH" ios/RNSkolenApp/Info.plist
+  plutil -replace CFBundleShortVersionString -string "$APPCENTER_BRANCH" $APPCENTER_SOURCE_DIRECTORY/ios/RNSkolenApp/Info.plist
 fi
 
 # Sett versjonen på Android
 if [ "$APPCENTER_BRANCH" != "master" ]; then
   echo "Setting Android version to $APPCENTER_BRANCH"
-  sed -i "s/versionName \".*\"/versionName \"$APPCENTER_BRANCH\"/" android/app/build.gradle
+  sed -i "s/versionName \".*\"/versionName \"$APPCENTER_BRANCH\"/" $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
 fi
