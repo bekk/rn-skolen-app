0ae2e87923fda5e0d5c64f71dc0d7aa638a4fe7c
Leif Erik Bjørkli <leiferikbjorkli@gmail.com>
2023-09-08 21:23:18 +0200

    👷‍♂️ Litt grunnleggende konfig av repo

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import codePush from 'react-native-code-push';
-import App from './App';
+import React from 'react'
+import codePush from 'react-native-code-push'
+import {App} from './App'
 
 function Main() {
-  return <App />;
+  return <App />
 }
 
 const codePushOptions = {
   updateDialog: true,
   installMode: codePush.InstallMode.IMMEDIATE,
-};
+}
 
-const CodePushEnabledMain = () => codePush(codePushOptions)(Main);
+const CodePushEnabledMain = () => codePush(codePushOptions)(Main)
 
-export default __DEV__ ? Main : CodePushEnabledMain();
+export default __DEV__ ? Main : CodePushEnabledMain()
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import Main from './app/Main';
-import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native'
+import Main from './app/Main'
+import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => Main)
